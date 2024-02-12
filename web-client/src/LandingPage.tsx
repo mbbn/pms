fba4f8e54169e1583c8e3c236898ff819f61fa87
Mohammad Biabani <biabani.mohammad@gmail.com>
2024-02-12 07:55:40 +0330

    feature: added locale and language

--- web-client/src/LandingPage.tsx
+++ web-client/src/LandingPage.tsx
@@ -1,107 +1,40 @@
 import * as React from 'react';
-import { PaletteMode } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
-import Divider from '@mui/material/Divider';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import ToggleButton from '@mui/material/ToggleButton';
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
-import AutoAwesomeRoundedIcon from '@mui/icons-material/AutoAwesomeRounded';
-import SvgMaterialDesign from "./icons/SvgMaterialDesign";
-import AppAppBar from './components/AppAppBar';
-import Hero from './components/Hero';
-import LogoCollection from './components/LogoCollection';
-import Highlights from './components/Highlights';
-import Pricing from './components/Pricing';
-import Features from './components/Features';
-import Testimonials from './components/Testimonials';
-import FAQ from './components/FAQ';
-import Footer from './components/Footer';
-import getLPTheme from './getLPTheme';
+import { CookiesProvider, useCookies } from "react-cookie";
+import {FormattedMessage, IntlProvider} from 'react-intl';
+import English from "./lang/en.json";
+import Persian from "./lang/fa.json";
+import getPMSTheme from './getPMSTheme';
+import {PaletteMode} from "@mui/material";
+import AppAppBar from "./components/AppAppBar";
 
-const defaultTheme = createTheme({});
-
-interface ToggleCustomThemeProps {
-    showCustomTheme: Boolean;
-    toggleCustomTheme: () => void;
-}
-
-function ToggleCustomTheme({
-                               showCustomTheme,
-                               toggleCustomTheme,
-                           }: ToggleCustomThemeProps) {
-    return (
-        <Box
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                width: '100dvw',
-                position: 'fixed',
-                bottom: 24,
-            }}
-        >
-            <ToggleButtonGroup
-                color="primary"
-                exclusive
-                value={showCustomTheme}
-                onChange={toggleCustomTheme}
-                aria-label="Platform"
-                sx={{
-                    backgroundColor: 'background.default',
-                    '& .Mui-selected': {
-                        pointerEvents: 'none',
-                    },
-                }}
-            >
-                <ToggleButton value>
-                    <AutoAwesomeRoundedIcon sx={{ fontSize: '20px', mr: 1 }} />
-                    Custom theme
-                </ToggleButton>
-                <ToggleButton value={false}>
-                    <SvgMaterialDesign sx={{ fontSize: '20px', mr: 1 }} />
-                    Material Design
-                </ToggleButton>
-            </ToggleButtonGroup>
-        </Box>
-    );
-}
 export default function LandingPage() {
+    const theme = createTheme(getPMSTheme());
     const [mode, setMode] = React.useState<PaletteMode>('dark');
-    const [showCustomTheme, setShowCustomTheme] = React.useState(true);
-    const LPtheme = createTheme(getLPTheme(mode));
+    const [cookies, setCookie] = useCookies(["locale"]);
+    const [locale, setLocale] = React.useState(cookies.locale ? cookies.locale : 'fa-IR');
+    const [lang, setLang] = React.useState(locale === 'en' ? English : Persian);
+    const handleLocale = (locale: string) => {
+        setLocale(locale);
+        setLang(locale === 'en' ? English : Persian);
+        setCookie("locale", locale, { path: "/", maxAge: 10000, secure: true })
+    }
+    if (cookies.locale === undefined) {
+        setCookie("locale", locale, { path: "/", maxAge: 10000, secure: true})
+    }
 
     const toggleColorMode = () => {
         setMode((prev) => (prev === 'dark' ? 'light' : 'dark'));
     };
 
-    const toggleCustomTheme = () => {
-        setShowCustomTheme((prev) => !prev);
-    };
-
     return (
-        <ThemeProvider theme={showCustomTheme ? LPtheme : defaultTheme}>
-            <CssBaseline />
-            <AppAppBar mode={mode} toggleColorMode={toggleColorMode} />
-            <Hero />
-            <Box sx={{ bgcolor: 'background.default' }}>
-                <LogoCollection />
-                <Features />
-                <Divider />
-                <Testimonials />
-                <Divider />
-                <Highlights />
-                <Divider />
-                <Pricing />
-                <Divider />
-                <FAQ />
-                <Divider />
-                <Footer />
-            </Box>
-            <ToggleCustomTheme
-                showCustomTheme={showCustomTheme}
-                toggleCustomTheme={toggleCustomTheme}
-            />
-        </ThemeProvider>
+        <IntlProvider locale={locale} messages={lang}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <AppAppBar mode={mode} toggleColorMode={toggleColorMode} />
+            </ThemeProvider>
+        </IntlProvider>
     );
 }
---
+++ web-client/src/getPMSTheme.tsx
@@ -0,0 +1,7 @@
+import type {} from '@mui/material/themeCssVarsAugmentation';
+import {ThemeOptions} from "@mui/material/styles";
+
+export default function getPMSTheme(): ThemeOptions {
+    return {
+    }
+}
