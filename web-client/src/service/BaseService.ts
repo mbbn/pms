46c442da6982fe0f770f401a8e309677fde8cb01
m.biabani <biabani.mohammad@gmail.com>
2024-02-26 09:32:36 +0330

    feature: create base service

--- web-client/src/service/BaseService.ts
+++ web-client/src/service/BaseService.ts
@@ -1,13 +1,15 @@
 export default class BaseService {
     get(path: string): Promise<Response> {
         return fetch(path, {
-            method: 'GET'
+            method: 'GET',
+            cache: 'no-cache'
         }).then((response)=>response.json());
     }
 
     post(path: string, body: any): Promise<Response> {
         return fetch(path, {
             method: 'POST',
+            cache: 'no-cache',
             body: JSON.stringify(body),
             headers:{
                 'Content-type': 'application/json; charset=UTF-8'
