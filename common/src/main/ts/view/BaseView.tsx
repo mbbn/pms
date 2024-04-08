80b3aabac32cd209e38fcb115e450e0ad65dbfe5
m.biabani <biabani.mohammad@gmail.com>
2024-04-08 15:45:03 +0330

    -

--- common/src/main/ts/view/BaseView.tsx
+++ common/src/main/ts/view/BaseView.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import {Formik} from 'formik';
 import BaseModel from "@common/model/BaseModel";
+import {getBaseMessage, getMessage} from "@common/provider/LocalProvider";
 import {BaseService} from "@common/service/BaseService";
 
 export interface ViewState<M extends BaseModel<I>, I> {
@@ -26,55 +26,21 @@ export default abstract class BaseView<M extends BaseModel<I>, I> extends React.
         super(props, context);
         this.service = service;
     }
-
-    /*getHistoryState(): ViewState<M> {
-        return this.shouldRestoreStateOnNavigateBack() && window.history.state && window.history.state[BaseView.VIEW_STATE_IN_HISTORY]
-        && window.history.state[BaseView.VIEW_POSITION_IN_HISTORY] !== window.history.length ? // Do not restore history state if user refreshes last page in history (in last page, user prefers to load fresh page)
-            this.reviseState(window.history.state[BaseView.VIEW_STATE_IN_HISTORY]) : null;
-    }*/
     render(): React.ReactNode {
         const renderContent = this.renderContent();
         if (!renderContent) {
             return null;
         }
-        return (
-            // <div id={this.getElementId()}>
-            //     <HotKeys keyMap={ShortcutHandler.getShortcutsMap(this.getShortcuts())} handlers={ShortcutHandler.getShortcutHandlers(this.getShortcuts())}>
-                    renderContent
-                // </HotKeys>
-                // {this.renderViewFooter()}
-            // </div>
-        );
+        return (renderContent);
     }
 
-    /*renderViewFooter(): React.ReactNode {
-        if (!UrlHandler.getShowFooterParam() || UserAgent.isMediumOrSmallerScreen()) {
-            return null;
-        }
-        return (
-            <div style={{width: '100%', backgroundColor: '#90caf9', padding: 20, marginTop: 10}}>
-                {[...this.getShortcuts(), ...this.getDefaultShortcuts()].map((shortcut, index) => <Button
-                    buttonType={new ButtonType(null, shortcut.getKeyAndTitle())} className="shortcut"
-                    onClick={shortcut.command} key={index}/>)}
-            </div>
-        );
-    }*/
-
     getModel(): M | null {
-        /*if (this.formik && !this.setStateIsInProgress) {
-            return this.getFormikModel();
-        } else {*/
-            return this.getStateModel();
-        // }
+        return this.getStateModel();
     }
 
     getStateModel(): M {
         return this.state.model;
     }
-
-    /*getFormikModel(): M | null {
-        return this.formik ? this.formik.state.values as M : null;
-    }*/
 
     setModel(model: M): void {
         this.setState({model});
@@ -84,9 +50,6 @@ export default abstract class BaseView<M extends BaseModel<I>, I> extends React.
         this.setStateIsInProgress = true;
         this.reviseState(state);
         super.setState(state, () => {
-            /*if (state.model && this.formik) {
-                this.formik.resetForm(state.model);
-            }*/
             this.setStateIsInProgress = false;
             if (callback) {
                 callback();
@@ -103,5 +66,9 @@ export default abstract class BaseView<M extends BaseModel<I>, I> extends React.
             state.model = this.createModel(state.model);
         }
         return state;
+    }
+
+    getBaseMessage(key: string) {
+        return getBaseMessage(key);
     }
 }
--- common/src/main/ts/view/SettingsView.tsx
+++ common/src/main/ts/view/SettingsView.tsx
@@ -1,13 +1,18 @@
+import { ReactNode } from "react";
 import BaseView from "@common/view/BaseView";
 import CompanyModel from "@common/model/CompanyModel";
 import CompanyService from "@common/service/CompanyService";
-import { ReactNode } from "react";
+import {Card, CardHeader, CardContent, Avatar} from "@mui/material";
+import {Settings} from "@mui/icons-material";
+import {getBaseMessage} from "@common/provider/LocalProvider";
 
 export default class SettingsView extends BaseView<CompanyModel, string> {
 
     service: CompanyService = new CompanyService();
     renderContent(): ReactNode {
-        return <>sssssssssssssssss</>;
+        return <Card variant="outlined">
+            <CardHeader title={getBaseMessage('settings')} avatar={<Avatar><Settings color="primary"/></Avatar>}/>
+        </Card>;
     }
     createModel(props?: any): CompanyModel {
         return new CompanyModel(props);
